3bd4508854b02b551a0f35ddd3ad0111ef506e7f
NWZX <nwzx@users.noreply.github.com>
2021-06-28 18:13:31 -0400

    ❌

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,263 +1,31 @@
-const _0x1f70 = [
-    'VjaGF0LTM3',
-    'dC5jb20iLC',
-    '9qZWN0SWQi',
-    'console',
-    'o1MzA0NjE5',
-    'cation',
-    'ZWFwcC5jb2',
-    'nction()\x20',
-    'App',
-    'bind',
-    '768KUHsSV',
-    'Jhc2Vpby5j',
-    '\x20Message\x20b',
-    'messaging.',
-    'prototype',
-    'onBackgrou',
-    'ltcGxlY2hh',
-    'NjM4YmJLbF',
-    'OUZWd2MiLC',
-    'dC0zN2RhNS',
-    'ndMessage',
-    '466eMsTVR',
-    '1822UOExeb',
-    'sage\x20',
-    'JtZXNzYWdp',
-    'BaeGMtemo0',
-    '113741hTLibZ',
-    'exception',
-    '/firebase-',
-    'log',
-    'UuYXBwc3Bv',
-    'return\x20/\x22\x20',
-    '[^\x20]+)+)+[',
-    'JJZCI6IjUz',
-    'dElkIjoiRy',
-    'YXQtMzdkYT',
-    'cElkIjoiMT',
-    'NzciLCJtZW',
-    '3yseoSr',
-    'kiOiJBSXph',
-    'bmdTZW5kZX',
-    'ctor(\x22retu',
-    'trace',
-    '243335DYwxuZ',
-    'w.gstatic.',
-    '1PUtews',
-    'length',
-    'I6Imh0dHBz',
-    '2755077NALAWt',
-    'ZGIuZmlyZW',
-    'MDQ2MTkxOD',
-    'registrati',
-    '+\x20this\x20+\x20\x22',
-    '71827EdmIyR',
-    '1BpgWkb',
-    'xlY2hhdC0z',
-    'parse',
-    'toString',
-    'U3lBbGpVMF',
-    '1669479fVyPkC',
-    'NpbXBsZWNo',
-    'RvcmFnZUJ1',
-    'b20iLCJwcm',
-    'messaging-',
-    'apply',
-    '^([^\x20]+(\x20+',
-    'warn',
-    'ZDEwMDI5MD',
-    'eyJhcGlLZX',
-    '{}.constru',
-    'initialize',
-    'constructo',
-    'test',
-    'rn\x20this\x22)(',
-    'messaging',
-    'itle',
-    '5maXJlYmFz',
-    'JhdXRoRG9t',
-    '2921fcrgKU',
-    'Background',
-    'Y2tldCI6In',
-    'return\x20(fu',
-    'error',
-    'UjBZIn0=',
-    'sejs/8.2.0',
-    'https://ww',
-    '\x20Message\x20T',
-    'logo.png',
-];
-const _0x151db8 = _0x2186;
-(function (_0x221be2, _0x5a0907) {
-    const _0x4b8153 = _0x2186;
-    while (!![]) {
-        try {
-            const _0x158ec6 =
-                parseInt(_0x4b8153(0x126)) * -parseInt(_0x4b8153(0x142)) +
-                parseInt(_0x4b8153(0x122)) * -parseInt(_0x4b8153(0x116)) +
-                -parseInt(_0x4b8153(0x137)) +
-                parseInt(_0x4b8153(0x132)) * parseInt(_0x4b8153(0x141)) +
-                -parseInt(_0x4b8153(0x15a)) * -parseInt(_0x4b8153(0x121)) +
-                -parseInt(_0x4b8153(0x147)) +
-                -parseInt(_0x4b8153(0x139)) * -parseInt(_0x4b8153(0x13c));
-            if (_0x158ec6 === _0x5a0907) break;
-            else _0x221be2['push'](_0x221be2['shift']());
-        } catch (_0x117b03) {
-            _0x221be2['push'](_0x221be2['shift']());
-        }
-    }
-})(_0x1f70, 0xdd2a5);
-const _0x47b00 = (function () {
-        let _0xc6bde2 = !![];
-        return function (_0x4ade97, _0x563ff4) {
-            const _0x59117d = _0xc6bde2
-                ? function () {
-                      const _0x52660a = _0x2186;
-                      if (_0x563ff4) {
-                          const _0xa9df2a = _0x563ff4[_0x52660a(0x14c)](_0x4ade97, arguments);
-                          return (_0x563ff4 = null), _0xa9df2a;
-                      }
-                  }
-                : function () {};
-            return (_0xc6bde2 = ![]), _0x59117d;
-        };
-    })(),
-    _0x29bc5d = _0x47b00(this, function () {
-        const _0x1bddf9 = function () {
-            const _0x3fc833 = _0x2186,
-                _0x225606 = _0x1bddf9[_0x3fc833(0x153) + 'r'](_0x3fc833(0x12b) + _0x3fc833(0x140) + '/')()[
-                    _0x3fc833(0x153) + 'r'
-                ](_0x3fc833(0x14d) + _0x3fc833(0x12c) + '^\x20]}');
-            return !_0x225606[_0x3fc833(0x154)](_0x29bc5d);
-        };
-        return _0x1bddf9();
-    });
-_0x29bc5d();
-const _0x43ce2d = (function () {
-        let _0x1b3840 = !![];
-        return function (_0xb9ebab, _0x23a840) {
-            const _0x521866 = _0x1b3840
-                ? function () {
-                      if (_0x23a840) {
-                          const _0x21d5cf = _0x23a840['apply'](_0xb9ebab, arguments);
-                          return (_0x23a840 = null), _0x21d5cf;
-                      }
-                  }
-                : function () {};
-            return (_0x1b3840 = ![]), _0x521866;
-        };
-    })(),
-    _0x447f12 = _0x43ce2d(this, function () {
-        const _0x6b48c = _0x2186,
-            _0x563a84 = function () {
-                const _0x454a40 = _0x2186;
-                let _0xb27dc8;
-                try {
-                    _0xb27dc8 = Function(
-                        _0x454a40(0x15d) +
-                            _0x454a40(0x113) +
-                            (_0x454a40(0x151) + _0x454a40(0x135) + _0x454a40(0x155) + '\x20)') +
-                            ');',
-                    )();
-                } catch (_0x3066d7) {
-                    _0xb27dc8 = window;
-                }
-                return _0xb27dc8;
-            },
-            _0x428bb2 = _0x563a84(),
-            _0x586e48 = (_0x428bb2['console'] = _0x428bb2[_0x6b48c(0x10f)] || {}),
-            _0x5cd6ed = ['log', _0x6b48c(0x14e), 'info', _0x6b48c(0x15e), _0x6b48c(0x127), 'table', _0x6b48c(0x136)];
-        for (let _0x496de9 = 0x0; _0x496de9 < _0x5cd6ed[_0x6b48c(0x13a)]; _0x496de9++) {
-            const _0x39c4a3 = _0x43ce2d[_0x6b48c(0x153) + 'r'][_0x6b48c(0x11a)][_0x6b48c(0x115)](_0x43ce2d),
-                _0x4516fe = _0x5cd6ed[_0x496de9],
-                _0x164067 = _0x586e48[_0x4516fe] || _0x39c4a3;
-            (_0x39c4a3['__proto__'] = _0x43ce2d[_0x6b48c(0x115)](_0x43ce2d)),
-                (_0x39c4a3[_0x6b48c(0x145)] = _0x164067[_0x6b48c(0x145)][_0x6b48c(0x115)](_0x164067)),
-                (_0x586e48[_0x4516fe] = _0x39c4a3);
-        }
-    });
-_0x447f12(),
-    importScripts(_0x151db8(0x161) + _0x151db8(0x138) + 'com/fireba' + _0x151db8(0x160) + _0x151db8(0x128) + 'app.js'),
-    importScripts(
-        _0x151db8(0x161) + _0x151db8(0x138) + 'com/fireba' + 'sejs/8.2.0' + _0x151db8(0x128) + _0x151db8(0x119) + 'js',
-    );
-function _0x2186(_0x215af7, _0x5a9fef) {
-    return (
-        (_0x2186 = function (_0x10bfb1, _0x447f12) {
-            _0x10bfb1 = _0x10bfb1 - 0x10f;
-            let _0x43ce2d = _0x1f70[_0x10bfb1];
-            return _0x43ce2d;
-        }),
-        _0x2186(_0x215af7, _0x5a9fef)
-    );
-}
-var firebaseConfig = JSON[_0x151db8(0x144)](
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+var firebaseConfig = JSON.parse(
     atob(
-        _0x151db8(0x150) +
-            _0x151db8(0x133) +
-            _0x151db8(0x146) +
-            _0x151db8(0x125) +
-            _0x151db8(0x11d) +
-            'hwZERNeDNw' +
-            _0x151db8(0x11e) +
-            _0x151db8(0x159) +
-            'YWluIjoic2' +
-            _0x151db8(0x11c) +
-            _0x151db8(0x11f) +
-            _0x151db8(0x158) +
-            _0x151db8(0x112) +
-            '0iLCJkYXRh' +
-            'YmFzZVVSTC' +
-            _0x151db8(0x13b) +
-            'Oi8vc2ltcG' +
-            _0x151db8(0x143) +
-            'N2RhNS1kZW' +
-            'ZhdWx0LXJ0' +
-            _0x151db8(0x13d) +
-            _0x151db8(0x117) +
-            _0x151db8(0x14a) +
-            _0x151db8(0x166) +
-            'OiJzaW1wbG' +
-            _0x151db8(0x164) +
-            'ZGE1Iiwic3' +
-            _0x151db8(0x149) +
-            _0x151db8(0x15c) +
-            _0x151db8(0x148) +
-            _0x151db8(0x12f) +
-            _0x151db8(0x12a) +
-            _0x151db8(0x165) +
-            _0x151db8(0x124) +
-            _0x151db8(0x134) +
-            _0x151db8(0x12d) +
-            _0x151db8(0x13e) +
-            'kzNCIsImFw' +
-            _0x151db8(0x130) +
-            _0x151db8(0x110) +
-            'MTg5MzQ6d2' +
-            'ViOjk1Y2Jh' +
-            _0x151db8(0x14f) +
-            'BiMWNkOTJi' +
-            _0x151db8(0x131) +
-            'FzdXJlbWVu' +
-            _0x151db8(0x12e) +
-            '1WTjNCR1I2' +
-            _0x151db8(0x15f),
+        'eyJhcGlLZXkiOiJBSXphU3lBbGpVMFBaeGMtemo0NjM4YmJLbFhwZERNeDNwOUZWd2MiLCJhdXRoRG9tYWluIjoic2ltcGxlY2hhdC0zN2RhNS5maXJlYmFzZWFwcC5jb20iLCJkYXRhYmFzZVVSTCI6Imh0dHBzOi8vc2ltcGxlY2hhdC0zN2RhNS1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20iLCJwcm9qZWN0SWQiOiJzaW1wbGVjaGF0LTM3ZGE1Iiwic3RvcmFnZUJ1Y2tldCI6InNpbXBsZWNoYXQtMzdkYTUuYXBwc3BvdC5jb20iLCJtZXNzYWdpbmdTZW5kZXJJZCI6IjUzMDQ2MTkxODkzNCIsImFwcElkIjoiMTo1MzA0NjE5MTg5MzQ6d2ViOjk1Y2JhZDEwMDI5MDBiMWNkOTJiNzciLCJtZWFzdXJlbWVudElkIjoiRy1WTjNCR1I2UjBZIn0=',
     ),
 );
-firebase[_0x151db8(0x152) + _0x151db8(0x114)](firebaseConfig);
-const messaging = firebase[_0x151db8(0x156)]();
-messaging[_0x151db8(0x11b) + _0x151db8(0x120)]((_0x334713) => {
-    const _0x3f8452 = _0x151db8;
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
     try {
-        console[_0x3f8452(0x129)](
-            '[firebase-' + _0x3f8452(0x14b) + 'sw.js]\x20Rec' + 'eived\x20back' + 'ground\x20mes' + _0x3f8452(0x123),
-            _0x334713,
-        );
-        const _0x2d2cca = 'Background' + _0x3f8452(0x162) + _0x3f8452(0x157),
-            _0x2d6524 = { body: _0x3f8452(0x15b) + _0x3f8452(0x118) + 'ody.', icon: '/firebase-' + _0x3f8452(0x163) };
-        self[_0x3f8452(0x13f) + 'on']['showNotifi' + _0x3f8452(0x111)](_0x2d2cca, _0x2d6524);
-    } catch (_0x357ff8) {
-        console[_0x3f8452(0x129)](_0x357ff8);
+        console.log('[firebase-messaging-sw.js] Received background message ', payload);
+        // Customize notification here
+        const notificationTitle = 'Background Message Title';
+        const notificationOptions = {
+            body: 'Background Message body.',
+            icon: '/firebase-logo.png',
+        };
+
+        self.registration.showNotification(notificationTitle, notificationOptions);
+    } catch (error) {
+        console.log(error);
     }
 });
