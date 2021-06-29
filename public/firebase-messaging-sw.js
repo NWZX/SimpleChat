fe9bde4269ece4d595abf6ae364f16f64d5e2af7
NWZX <nwzx@users.noreply.github.com>
2021-06-28 22:23:56 -0400

    adapt to firebase hosting

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,11 @@
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
-
-// Initialize the Firebase app in the service worker by passing the generated config
-var firebaseConfig = JSON.parse(
-    atob(
-        'eyJhcGlLZXkiOiJBSXphU3lBbGpVMFBaeGMtemo0NjM4YmJLbFhwZERNeDNwOUZWd2MiLCJhdXRoRG9tYWluIjoic2ltcGxlY2hhdC0zN2RhNS5maXJlYmFzZWFwcC5jb20iLCJkYXRhYmFzZVVSTCI6Imh0dHBzOi8vc2ltcGxlY2hhdC0zN2RhNS1kZWZhdWx0LXJ0ZGIuZmlyZWJhc2Vpby5jb20iLCJwcm9qZWN0SWQiOiJzaW1wbGVjaGF0LTM3ZGE1Iiwic3RvcmFnZUJ1Y2tldCI6InNpbXBsZWNoYXQtMzdkYTUuYXBwc3BvdC5jb20iLCJtZXNzYWdpbmdTZW5kZXJJZCI6IjUzMDQ2MTkxODkzNCIsImFwcElkIjoiMTo1MzA0NjE5MTg5MzQ6d2ViOjk1Y2JhZDEwMDI5MDBiMWNkOTJiNzciLCJtZWFzdXJlbWVudElkIjoiRy1WTjNCR1I2UjBZIn0=',
-    ),
-);
-
-firebase.initializeApp(firebaseConfig);
+importScripts('/__/firebase/8.6.8/firebase-app.js');
+importScripts('/__/firebase/8.6.8/firebase-messaging.js');
+importScripts('/__/firebase/init.js');
 
 // Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(async (payload) => {
+messaging.onBackgroundMessage(async function (payload) {
     try {
         console.log('[firebase-messaging-sw.js] Received background message ', payload);
         // Customize notification here
